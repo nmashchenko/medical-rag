05985c79a4e806cbad1a44e7992c7995e814603a
Brad Zeng <brad.zeng2015@gmail.com>
2025-05-06 19:52:20 -0500

    add documentation and return url instead of pdf name

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,7 @@ import numpy as np
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+#load paper data and embeddings
 with open('pdf_chunks.json', 'r') as f:
     all_chunks = json.load(f)
 embedding = np.load("embeddings.npy", allow_pickle=True)
@@ -18,20 +19,25 @@ def get_papers():
     global all_chunks, embedding, embedder
     message = request.get_json()
     message_content = message["content"]
+    
+    # get embedding of query
     message_embedding = embedder.embed_text([message_content])
     best_chunk = {"similarity_score" : 0}
+    
+    # find chunk with highest similarity score
     for i in range(len(all_chunks)):
         curr_chunk = all_chunks[i]
         similarity_score = get_similarity(message_embedding, embedding[i])
         if similarity_score > best_chunk["similarity_score"]:
             curr_chunk["similarity_score"] = similarity_score
+            # print(f"Similarity: {similarity_score}, Source: {curr_chunk['source']}, Index: {curr_chunk['chunk_index']}")
             best_chunk = curr_chunk
 
     return_value = {
         "id": message["id"],
         "role": "assistant",
         "content": (
-            f"Source: {best_chunk['source']}\n"
+            f"Source: http://arxiv.org/pdf/{best_chunk['source'][:-4]}\n"
             f"Similarity: {best_chunk['similarity_score']}\n\n"
             f"{best_chunk['text']}"
         ),
--- backend/embedding.py
+++ backend/embedding.py
@@ -14,6 +14,15 @@ class Embedding:
         self.batch_size = batch_size
         
     def embed_text(self, data):
+        """
+        Create embeddings of text data
+
+        Parameters:
+            data (np.array): np array of strings
+
+        Returns:
+            embedding of given data
+        """
         encoded_inputs = self.tokenizer(data, padding=True, truncation=True, max_length=512, return_tensors="pt")
         input_ids = encoded_inputs["input_ids"]
         attention_mask = encoded_inputs["attention_mask"]
@@ -26,6 +35,15 @@ class Embedding:
         return batch_embeddings
 
     def create_embeddings(self, chunks):
+        """
+        Create embeddings in batches given chunks and saves to .npy file
+
+        Parameters:
+            chunks: list of chunk data (dict)
+
+        Returns:
+            list of embeddings
+        """
         texts = [chunk["text"] for chunk in chunks]
         embeddings = []
         for i in range(0, len(chunks), self.batch_size):
@@ -37,6 +55,17 @@ class Embedding:
         return embeddings
             
 def get_similarity(em1, em2):
+    """
+    Get cosine similarity of two embeddings
+
+    Parameters:
+        em1 (torch.Tensor or np.array): First embedding
+        em2 (torch.Tensor or np.array): Second embedding
+
+    Returns:
+        cosine similarity of em1 and em2
+    """
+    # convert embeddings to np array
     em1 = em1.detach().numpy() if isinstance(em1, torch.Tensor) else em1
     em2 = em2.detach().numpy() if isinstance(em2, torch.Tensor) else em2
     return float(cosine_similarity(em1.reshape(1,-1), em2.reshape(1,-1))[0][0])
